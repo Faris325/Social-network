0c96c9313eac9150b4cf55414509bef9e96eb84b
i <Vlad.getmanov.2014@gmail.com>
2025-10-11 00:15:42 +0300

    Форма авторизации

--- sh/settings.py
+++ sh/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME':'sh',
         'USER':'postgres',
         'PASSWORD':'1234',
-        'HOST':'db',
+        'HOST':'localhost',
         'PORT':'5432',
                    
     }
@@ -169,7 +169,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)], 
+            "hosts": [("127.0.0.1", 6379)], 
         },
     },
 }
